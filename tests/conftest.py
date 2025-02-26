b698cbe19b9906fcbd37869ad5feb6a3cf35c955
Lucas Terriel <ls.terriel@gmail.com>
2025-02-26 13:58:02 +0100

    update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,40 @@
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 from api.database import BASE
 
+
+
+"""
 @pytest.fixture()
 def engine():
-    return create_engine("sqlite:///test.db", connect_args={"check_same_thread": False})
+    return create_engine("sqlite:///:memory:",
+                         connect_args={"check_same_thread": False}
+                         )
+
 
 @pytest.fixture()
 def tables(engine):
     BASE.metadata.create_all(engine)
     yield
     BASE.metadata.drop_all(engine)
+"""
 
-@pytest.fixture()
-def session(engine, tables):
+@pytest.fixture(scope="function")
+def session():
+    engine = create_engine("sqlite:///:memory:",
+                           connect_args={"check_same_thread": False},
+                           poolclass=StaticPool,
+                           echo=True
+                           )
+
+    # TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+    BASE.metadata.create_all(bind=engine)
     connection = engine.connect()
     transaction = connection.begin()
     Session = sessionmaker(bind=connection, autoflush=False, autocommit=False)
     session = Session()
-
 
     try:
         yield session
